cb501fd3e6ebe2c4ff8243460f738a3a576020e0
Slava <ja227@mail.ru>
2023-05-12 22:05:41 +0300

    EndOfThe10THday

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-/* jshint esversion: 11 */
 let currentRankName = '';
 
 function init() {
-  const randomIndex = Math.floor(Math.random()*(store.length));
+  console.log(currentRankName);
+  const randomIndex = Math.floor(Math.random() * (store.length));
   const rank = store[randomIndex];
 
   currentRankName = rank.name;
 
-    
   document.getElementById('image').src = rank.link;
 }
+
+
 window.onload = init;
+
+
+
 
 function generate() {
   const selectedColour = document.querySelector('input[name="color"]:checked').value;
@@ -25,7 +29,7 @@ function generate() {
   const pattern = document.getElementById("pattern").checked;
 
 
-  const foundRank = store.find((obj)  => {
+  const foundRank = store.find((obj) => {
     return obj.color === selectedColour &&
       obj.stripes === countOfStripes &&
       obj.stars === countOfStars &&
@@ -33,8 +37,8 @@ function generate() {
       obj.stripesType === stripesType &&
       obj.stripesThick === stripesThick &&
       obj.emblem === emblem &&
-      obj.edging === edging&&
-      obj.wreath === wreath&&
+      obj.edging === edging &&
+      obj.wreath === wreath &&
       obj.pattern === pattern;
   });
 
@@ -42,295 +46,293 @@ function generate() {
 
   if (currentRankName !== foundRank?.name) {
     alert('не найдено');
-  }
-  else {
+  } else {
     document.getElementById('label').value = foundRank.name;
     alert('найдено');
   }
 }
 
 
-const store = [
-  {
-    name: 'Рядовой',
-    link: '../images/1ryadovoy.svg',
-    color: 'red',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Ефрейтор',
-    link: '../images/2efreytor.svg',
-    color: 'red',
-    stripes: 1,
-    stripesType: 'across',
-    stripesThick: 'thin',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Младший сержант',
-    link: '../images/3mlSerjant.svg',
-    color: 'red',
-    stripes: 2,
-    stripesType: 'across',
-    stripesThick: 'thin',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false 
-  },
-  {
-    name: 'Сержант',
-    link: '../images/4serjant.svg',
-    color: 'red',
-    stripes: 3,
-    stripesType: 'across',
-    stripesThick: 'thin',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Старший сержант',
-    link: '../images/5stSerjant.svg',
-    color: 'red',
-    stripes: 1,
-    stripesType: 'across',
-    stripesThick: 'thick',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Старшина',
-    link: '../images/6starshina.svg',
-    color: 'red',
-    stripes: 1,
-    stripesType: 'along',
-    stripesThick: 'thick',
-    stars: 0,
-    starsType: 'no',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Прапорщик',
-    link: '../images/7praporshick.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 2,
-    starsType: 'small',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Старший прапорщик',
-    link: '../images/8stPraporshick.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 3,
-    starsType: 'small',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Младший лейтенант',
-    link: '../images/9mlLetenant.svg',
-    color: 'green',
-    stripes: 1,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 1,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Лейтенант',
-    link: '../images/10letenant.svg',
-    color: 'green',
-    stripes: 1,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 2,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Старший лейтенант',
-    link: '../images/11stLetenant.svg',
-    color: 'green',
-    stripes: 1,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 3,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Капитан',
-    link: '../images/12captain.svg',
-    color: 'green',
-    stripes: 1,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 4,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Майор',
-    link: '../images/13mayor.svg',
-    color: 'green',
-    stripes: 2,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 1,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Подполковник',
-    link: '../images/14podpolkovnick.svg',
-    color: 'green',
-    stripes: 2,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 2,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Полковник',
-    link: '../images/15polkovnick.svg',
-    color: 'green',
-    stripes: 2,
-    stripesType: 'along',
-    stripesThick: 'thin',
-    stars: 3,
-    starsType: 'small',
-    emblem: false,
-    edging: false,
-    wreath: false,
-    pattern: false
-  },
-  {
-    name: 'Генерал-майор',
-    link: '../images/16generalMajor.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 1,
-    starsType: 'small',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: true
-  },
-  {
-    name: 'Генерал-лейтенант',
-    link: '../images/17generalLetenant.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 2,
-    starsType: 'small',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: true
-  },
-  {
-    name: 'Генерал-полковник',
-    link: '../images/18generalPolkovnick.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 3,
-    starsType: 'small',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: true
-  },
-  {
-    name: 'Генерал армии',
-    link: '../images/19generalOfArmy.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 1,
-    starsType: 'big',
-    emblem: false,
-    edging: true,
-    wreath: false,
-    pattern: true
-  },
-  {
-    name: 'Маршал Российской Федерации',
-    link: '../images/20marshallRF.svg',
-    color: 'green',
-    stripes: 0,
-    stripesType: 'no',
-    stripesThick: 'no',
-    stars: 1,
-    starsType: 'big',
-    emblem: true,
-    edging: true,
-    wreath: false,
-    pattern: true
-  },
-  
-  
+const store = [{
+  name: 'Рядовой',
+  link: '../images/1ryadovoy.svg',
+  color: 'red',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Ефрейтор',
+  link: '../images/2efreytor.svg',
+  color: 'red',
+  stripes: 1,
+  stripesType: 'across',
+  stripesThick: 'thin',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Младший сержант',
+  link: '../images/3mlSerjant.svg',
+  color: 'red',
+  stripes: 2,
+  stripesType: 'across',
+  stripesThick: 'thin',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Сержант',
+  link: '../images/4serjant.svg',
+  color: 'red',
+  stripes: 3,
+  stripesType: 'across',
+  stripesThick: 'thin',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Старший сержант',
+  link: '../images/5stSerjant.svg',
+  color: 'red',
+  stripes: 1,
+  stripesType: 'across',
+  stripesThick: 'thick',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Старшина',
+  link: '../images/6starshina.svg',
+  color: 'red',
+  stripes: 1,
+  stripesType: 'along',
+  stripesThick: 'thick',
+  stars: 0,
+  starsType: 'no',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Прапорщик',
+  link: '../images/7praporshick.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 2,
+  starsType: 'small',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Старший прапорщик',
+  link: '../images/8stPraporshick.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 3,
+  starsType: 'small',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Младший лейтенант',
+  link: '../images/9mlLetenant.svg',
+  color: 'green',
+  stripes: 1,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 1,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Лейтенант',
+  link: '../images/10letenant.svg',
+  color: 'green',
+  stripes: 1,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 2,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Старший лейтенант',
+  link: '../images/11stLetenant.svg',
+  color: 'green',
+  stripes: 1,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 3,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Капитан',
+  link: '../images/12captain.svg',
+  color: 'green',
+  stripes: 1,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 4,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Майор',
+  link: '../images/13mayor.svg',
+  color: 'green',
+  stripes: 2,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 1,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Подполковник',
+  link: '../images/14podpolkovnick.svg',
+  color: 'green',
+  stripes: 2,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 2,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Полковник',
+  link: '../images/15polkovnick.svg',
+  color: 'green',
+  stripes: 2,
+  stripesType: 'along',
+  stripesThick: 'thin',
+  stars: 3,
+  starsType: 'small',
+  emblem: false,
+  edging: false,
+  wreath: false,
+  pattern: false
+},
+{
+  name: 'Генерал-майор',
+  link: '../images/16generalMajor.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 1,
+  starsType: 'small',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: true
+},
+{
+  name: 'Генерал-лейтенант',
+  link: '../images/17generalLetenant.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 2,
+  starsType: 'small',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: true
+},
+{
+  name: 'Генерал-полковник',
+  link: '../images/18generalPolkovnick.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 3,
+  starsType: 'small',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: true
+},
+{
+  name: 'Генерал армии',
+  link: '../images/19generalOfArmy.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 1,
+  starsType: 'big',
+  emblem: false,
+  edging: true,
+  wreath: false,
+  pattern: true
+},
+{
+  name: 'Маршал Российской Федерации',
+  link: '../images/20marshallRF.svg',
+  color: 'green',
+  stripes: 0,
+  stripesType: 'no',
+  stripesThick: 'no',
+  stars: 1,
+  starsType: 'big',
+  emblem: true,
+  edging: true,
+  wreath: false,
+  pattern: true
+},
+
+
 ];
